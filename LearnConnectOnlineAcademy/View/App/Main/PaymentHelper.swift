d2276b290e24fdaea2e5458e5cb445dcae37b66f
HudahanAltn <dev.hudahanaltun@gmail.com>
2024-11-25 00:18:49 +0300

    kullanıcının kursları sepete eklemesi, ödeme yapıldıktan sonra kursların satın alınma işlemi tamamlandı. Satın alınan kursların eğitim sayfasında görüntülenmesi sağlandı.

---
+++ LearnConnectOnlineAcademy/View/App/Main/PaymentHelper.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentHelper.swift
+//  LearnConnectOnlineAcademy
+//
+//  Created by Hüdahan Altun on 24.11.2024.
+//
+
+import Foundation
---
+++ LearnConnectOnlineAcademy/View/Cells/Main/CoursesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesTableViewCell.swift
+//  LearnConnectOnlineAcademy
+//
+//  Created by Hüdahan Altun on 24.11.2024.
+//
+
+import UIKit
+
+class CoursesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
