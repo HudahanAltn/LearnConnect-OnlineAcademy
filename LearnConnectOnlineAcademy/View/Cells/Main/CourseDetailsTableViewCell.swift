c7267f16b66933b88ec054f1bc6ac7ea6e42e9c5
HudahanAltn <dev.hudahanaltun@gmail.com>
2024-11-25 19:12:17 +0300

    video oynatma sayfası tasarımı yapıldı.

---
+++ LearnConnectOnlineAcademy/View/Cells/Main/CourseDetailsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CourseDetailsTableViewCell.swift
+//  LearnConnectOnlineAcademy
+//
+//  Created by Hüdahan Altun on 25.11.2024.
+//
+
+import UIKit
+
+class CourseDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
