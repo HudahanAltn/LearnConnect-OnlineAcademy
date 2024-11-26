97ebcd7f5acffe26606ebd65275a16d06176dff6
HudahanAltn <dev.hudahanaltun@gmail.com>
2024-11-26 22:28:26 +0300

    Video offline yönetimi öncesi

---
+++ LearnConnectOnlineAcademy/View/Cells/Main/ItemReviewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemReviewsTableViewCell.swift
+//  LearnConnectOnlineAcademy
+//
+//  Created by Hüdahan Altun on 26.11.2024.
+//
+
+import UIKit
+
+class ItemReviewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
