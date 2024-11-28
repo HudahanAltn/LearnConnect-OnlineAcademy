9ef86bdd30c8fc4c5ac5e290b1b57ccf0f5395e1
HudahanAltn <dev.hudahanaltun@gmail.com>
2024-11-28 17:07:39 +0300

    Kurs izleme sayfasında bazı buglar giderildi.

---
+++ LearnConnectOnlineAcademyTests/LearnConnectOnlineAcademyTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearnConnectOnlineAcademyTests.swift
+//  LearnConnectOnlineAcademyTests
+//
+//  Created by Hüdahan Altun on 28.11.2024.
+//
+
+import Testing
+
+struct LearnConnectOnlineAcademyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ LearnConnectOnlineAcademyTests/LearnConnectTest.swift
@@ -0,0 +1,8 @@
+//
+//  LearnConnectTest.swift
+//  LearnConnectOnlineAcademyTests
+//
+//  Created by Hüdahan Altun on 28.11.2024.
+//
+
+import Foundation
